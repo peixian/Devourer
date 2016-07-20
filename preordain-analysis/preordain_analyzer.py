a74903ef9d28764e81bc47b1a012d6f7fcd19e48
Peixian Wang <peixian@fastmail.com>
2016-07-20 17:08:15 -0500

    Wrote new documentation for the methods

--- preordain-analysis/preordain_analyzer.py
+++ preordain-analysis/preordain_analyzer.py
@@ -1,5 +1,3 @@
-#TODO Clean up the documentation! 
-
 import sys
 import json
 import requests
@@ -10,7 +8,7 @@ import datetime
 import sqlite3
 import hashlib
 
-DATA_PATH = '/' #TODO FIX THIS
+DATA_PATH = '.' #TODO in current directory while testing, needs to be fixed before shipping!
 
 HS_JSON = 'https://api.hearthstonejson.com/v1/latest/enUS/'
 HS_JSON_EXT = ['cardbacks.json', 'cards.collectible.json', 'cards.json']
@@ -30,7 +28,14 @@ class preordain_analyzer(object):
 
     def grab_data(self, username, api_key):
         '''
-        Use this method, pull_data and parse_data are both outdated and require mulitude of files
+        Grabs the data from the trackobot servers, writes it out to a new files and the database if it doesn't exist/outdated
+
+        Keyword arguments:
+        username -- str, trackobot username
+        api_key -- str, trackobot api_key
+
+        Returns:
+        The contents of the json_file (includes history & metadata)
         '''
         self.username = username
         self.api_key = api_key
@@ -38,7 +43,7 @@ class preordain_analyzer(object):
         auth = {'username': username, 'token': api_key}
         req = requests.get(url, params=auth).json()
         metadata = req['meta']
-        count, json_name, hdf5name = self.store_data()
+        count, json_name, hdf5name = self.store_data(total_items)
         #if it's not equal, repull
         if metadata['total_items'] != count:
             results = {'children': req['history']}
@@ -50,6 +55,8 @@ class preordain_analyzer(object):
             self.history = results
             self.generate_decks()
             self.write_hdf5(hdf5_name)
+            with open('{}{}'.format(DATA_PATH, json_name), "w") as outfile:
+                json.dump(results, outfile)
         else:
             results = self.read_data(json_name, hdf5_name)
         return results
@@ -57,6 +64,9 @@ class preordain_analyzer(object):
     def generate_decks(self):
         '''
         Differentiates between the different deck types, and sorts them into their individual lists (history is a massive array, transform into a pandas dataframe for processing)
+
+        Returns:
+        Pandas dataframe with all the games
         '''
         self.games = pd.DataFrame(self.history['children'])
         self.games.loc[self.games['hero_deck'].isnull(), 'hero_deck'] = 'Other'
@@ -70,6 +80,7 @@ class preordain_analyzer(object):
 
 
     def _make_dates(self):
+        '''Internal method -- Converts the dates in self.games to separate columns for easier parsing, called by generate_decks'''
         format_date = lambda x: datetime.datetime.strptime(x[:-5], '%Y-%m-%dT%H:%M:S')
         split_date = lambda x: {'year': x.year, 'month': x.month, 'day': x.day, 'hour': x.hour, 'minute': x.minute, 'second': x.second}
         date_df = pd.DataFrame(list(map(lambda x: split_date(format_date(x)), self.gaes['added'])))
@@ -77,24 +88,34 @@ class preordain_analyzer(object):
 
     def _get_card_list(self, dict_list, player='me'):
         '''
-        Returns the list of cards that were played in a game
+        Internal method -- Returns the list of cards that were played in a game, called by _generate_cards_played
+
+        Keyword arguments:
+        dict_list -- list of dictionaries from the ['card_history'] column in self.games for one particular game
+        player -- the player to be parsing
+
+        Returns:
+        p_card_list -- array of card names (str)
         '''
         p_card_list = list(filter(None, map(lambda x: x['card']['name'] if x['player'] == player else None, dict_list)))
         return p_card_list
 
 
     def _generate_cards_played(self):
-        '''
-        Generates a list of cards for player and opponent into the list ['p_cards_played'] and ['o_cards_played'], meant to be used with the generate_cards function, this function is automatically called by generate_decks
-        '''
+        '''Internal method -- Generates a list of cards for player and opponent into the list ['p_cards_played'] and ['o_cards_played'], called by generate_decks'''
         self.games['p_cards_played'] = self.games['card_history'].map(lambda x: self._get_card_list(x, player='me'))
         self.games['o_cards_played'] = self.games['card_history'].map(lambda x: self._get_card_list(x, player='opponent'))
 
     def generate_matchups(self, game_mode = 'ranked', game_threshold = 0):
         '''
-        Generates a pandas groupby table with duration, count, coin, win #, and win %
-        game_mode is either ranked, casual, or both
-        game_threshold filters out the games with lower threshold
+        Generates a pandas groupby table with duration, count, coin, win #, win%, and card_history
+
+        Keyword arguments:
+        game_mode -- str, either 'ranked', 'casual', or 'both', default is ranked
+        game_threshold -- lowerbound for games played, any # of games lower than the threshold are not returned
+
+        Returns:
+        grouped -- pandas groupby object, indicies are player deck 'p_deck_type' then opponent 'o_deck_type'
         '''
         decks = self.games
         if game_mode != 'both':
@@ -110,7 +131,13 @@ class preordain_analyzer(object):
     def generate_cards(self, filtered):
         '''
         Generates a grouped win/loss count for specific cards
-        filtered should be a filtered subset of self.games, using some combination of rank/deck/hero
+
+        Keyword arguments:
+        filtered -- pandas dataframe, should be a subset of self.games filtered somehow
+
+        Returns:
+        p_df -- pandas groupby object, cards marked as 'me' for player, index is the card name ['card'], columns are win count and loss count ['win', 'loss']
+        o_df -- pandas groupby object, cards marked as 'opponent' for player, index is the card name ['card'], columns are win count and loss count ['win', 'loss']
         '''
         p_df = []
         o_df = []
@@ -129,15 +156,25 @@ class preordain_analyzer(object):
     def write_hdf5(self, hdf5_name):
         '''
         Writes out self.games into a hdf5_file
+
+        Keyword arguments:
+        hdf5_name -- str, name of the hdf5 file
         '''
         self.games.to_hdf('{}{}'.format(DATA_PATH, hdf5_name), 'table', append = False)
 
-    def store_data(self):
+    def store_data(self, total_items):
         '''
         Stores the python data by using the filename as the sha5 hash of the username and api_key -> hash is stored in a database for lookups later, data is stored using the hdf5 format
         Table is in the format of ['user_hash', 'total_items', 'json_name', 'hdf5_name']
-        '''
 
+        Keyword arguments:
+        total_items -- int, total number of items given by the trackobot api
+
+        Returns:
+        user[1] -- int, total items
+        user[2] -- str, json_name
+        user[3] -- str, hdf5_name
+        '''
         user_hash = hashlib.sha1(b'{}{}'.format(self.username, self.api_key)).hexdigest()
         conn = sqlite3.connect('{}/userdata.db'.format(DATA_PATH)) #TODO FIX THIS
         c = conn.cursor()
@@ -145,7 +182,7 @@ class preordain_analyzer(object):
             c.execute('SELECT * FROM users WHERE user_hash=?', user_hash)
             user = c.fetchone()
         else:
-            user = (user_hash, history['meta']['total_items'], '{}_j.json'.format(user_hash), '{{}_h.hdf5}'.format(user_hash))
+            user = (user_hash, total_items, '{}_j.json'.format(user_hash), '{{}_h.hdf5}'.format(user_hash))
             c.execute('INSERT INTO users VALUES ?', user)
         conn.commit()
         conn.close()
@@ -154,6 +191,13 @@ class preordain_analyzer(object):
     def read_data(self, json_name, hdf5_name):
         '''
         Takes the names of the files and loads them into memory for processing
+
+        Keyword arguments:
+        json_name -- str, name of the json file
+        hdf5_name -- str, name of the hdf5 file
+
+        Returns:
+        results -- dict, complete history of games and metadata
         '''
         results = json.load('{}{}'.format(DATA_PATH, json_name))
         self.history = results

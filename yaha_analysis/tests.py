02315366e7a0f0a85d53091a6857804475763b9c
Peixian Wang <peixian@fastmail.com>
2016-08-01 10:32:24 -0500

    Readme + tests changes

--- yaha_analysis/tests.py
+++ yaha_analysis/tests.py
@@ -56,4 +56,11 @@ class YahaTests(TestCase):
         self.assertTrue(len(card_types) > 0)
         self.assertTrue(isinstance(card_types[0], str))
 
-        
+    def get_card_list_test(self):
+        self.client.open_collectobot_data()
+        self.client.generate_decks()
+        dict_list_test = [{"player":"opponent","turn":1,"card":{"id":"GAME_005","name":"The Coin","mana":null}},{"player":"opponent","turn":1,"card":{"id":"EX1_169","name":"Innervate","mana":0}},{"player":"opponent","turn":1,"card":{"id":"AT_043","name":"Astral Communion","mana":4}},{"player":"opponent","turn":1,"card":{"id":"CS2_017","name":"Shapeshift","mana":2}},{"player":"me","turn":2,"card":{"id":"EX1_169","name":"Innervate","mana":0}},{"player":"me","turn":2,"card":{"id":"NEW1_026","name":"Violet Teacher","mana":4}},{"player":"opponent","turn":2,"card":{"id":"OG_202","name":"Mire Keeper","mana":4}},{"player":"opponent","turn":2,"card":{"id":"CS2_017","name":"Shapeshift","mana":2}},{"player":"me","turn":3,"card":{"id":"LOE_115","name":"Raven Idol","mana":1}},{"player":"me","turn":3,"card":{"id":"AT_037","name":"Living Roots","mana":1}}]
+        p_card_list = self.client._get_card_list(dict_list = dict_list_test)
+        self.assertTrue(len(p_card_list) > 0)
+        self.assertTrue(isinstance(p_card_list[0], str))
+
